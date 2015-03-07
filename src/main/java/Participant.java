ee4aa528612ac7718ab8efa3e4bbcf40364749ca
peteaq <peteaq@PeTeAQ-Linux-Laptop>
2015-03-07 15:38:06 +0100

    Project cleaned.

---
+++ src/main/java/Participant.java
@@ -0,0 +1,21 @@
+
+public class Participant {
+
+	String name;
+	String surname;
+	String email;
+	String company;
+	String origin;
+	String position;
+	
+	Participant (String name, String surname, String email, String company, String origin, String position) 
+	{
+		this.name = name;
+		this.surname = surname;
+		this.email = email;
+		this.company = company;
+		this.origin = origin;
+		this.position = position;
+	}
+
+}
